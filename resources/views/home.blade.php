9b950c92d2badde34cf33a93823eb83229f3ba69
InfoSaqib <hafizsaqi555@gmail.com>
2026-03-30 12:29:07 +0500

    feat: implement basic routing

---
+++ resources/views/home.blade.php
@@ -0,0 +1,3 @@
+<h1 class="text-center text-red-500">HOME page</h1>
+<h2>{{ $name }}</h2>
+<a href="{{ route('welcome') }}">Welcome</a>
--- routes/web.php
+++ routes/web.php
@@ -4,4 +4,13 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('welcome');
+//Redirection
+Route::redirect('/welcome', '/');
+
+// Route::view('/home', 'home');
+Route::view('/about', 'about')->name('about');
+
+Route::get('/home/{name?}', function ($name = null) {
+    return view('home', ['name' => $name]);
+})->name('home');
