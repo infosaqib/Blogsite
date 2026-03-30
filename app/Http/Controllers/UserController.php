6b647dcfb599d88395c3429673f1a9ab169c8b1b
InfoSaqib <hafizsaqi555@gmail.com>
2026-03-30 14:31:51 +0500

    feat: implement view features

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,15 +3,21 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\View;
 
 class UserController extends Controller
 {
     public function getUser($name)
     {
-        return view('user', ['name' => $name]);
+        $users = ['bilal', 'zain', 'uzair'];
+        return view('user', ['name' => $name, 'users' => $users]);
     }
     public function login()
     {
-        return view('admin.login');
+        if (View::exists('admin.login')) {
+            return view('admin.login');
+        } else {
+            echo 'No View Found';
+        }
     }
 }
--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -1,3 +1,12 @@
-<h1>
-    User is {{ $name }}
-</h1>
+<h3>User id: {{ rand() }}</h3>
+@if($name === 'joe')
+    <h1>User is {{ $name }}</h1>
+@elseif($name === 'osama')
+    <h1>User is osama</h1>
+@else
+    <h1>other user</h1>
+@endif
+
+@foreach($users as $user)
+    <h2>{{ $user  }}</h2>
+@endforeach
