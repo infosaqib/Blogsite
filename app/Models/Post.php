311f4a79f4c62c939e2c9a2eedb1e1a56f264273
InfoSaqib <hafizsaqi555@gmail.com>
2026-05-06 12:37:39 +0500

    feat: setters AND getters

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -16,4 +16,13 @@ class Post extends Model
     {
         return $this->belongsTo(User::class);
     }
+
+    //Acessors (Getters) & Mutators (Setters)
+    protected function title(): Attribute
+    {
+        return Attribute::make(
+            get: fn($value) => strtoupper($value),
+            set: fn($value) => strtolower($value)
+        );
+    }
 }
